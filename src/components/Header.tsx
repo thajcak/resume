943463bef85cbf69a7da28b95c4b0cb4dba2a2f7
Thomas Hajcak <thomas.hajcak@gmail.com>
2024-12-19 11:14:09 -0500

    Header Fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export const Header = () => {
   return (
     <div className="header">
-      <span className="name">Thomas Hajcak</span>
-      <hr/>
+      <div className="name">Thomas Hajcak</div>
       <div className="title">Salesforce Engineer</div>
       <div className="sub-title">AI Enthusiast | UX Advocate</div>
     </div>
